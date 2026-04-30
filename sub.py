3ec01eb98265a082b618e13fc943bce15d6e9eff
anikavaishu73-netizen <anikavaishu73@gmail.com>
2026-04-30 08:26:31 +0530

    Create sub.py

---
+++ sub.py
@@ -0,0 +1,11 @@
+s1=input("Enter first String : ");
+s2=input("Enter second String : ");
+print("First string is : ",s1);
+print("Second string is : ",s2);
+print("concatenations of two strings :",s1+s2);
+print("Substring of given string :",s1[1:4]);
+output:
+Enter first String : ani
+Enter second String : vinu
+First string is :  ani
+Second string is :  vinu
